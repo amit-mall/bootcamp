b8454338c3694295a2b0f91fb53776e4c6d11750
amit.mall <amit.mall@tothenew.com>
2025-02-19 14:03:24 +0530

    reactjs day 2

--- nextjs/reactjs/simple-app/src/components/classComponent.js
+++ nextjs/reactjs/simple-app/src/components/classComponent.js
@@ -1,6 +1,6 @@
 import React, {Component} from "react"
 
-export default class ClassComponent extends Component{
+export  default class ClassComponent extends Component{
     render(){
         return(
             <>
